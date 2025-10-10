256d1cecdc82b93a7c04e62786a9a4795999db7e
Kekoeur <Christo26.g@gmail.com>
2025-10-10 11:11:15 +0200

    error npm build

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+// ./config/plugins.ts`
+module.exports = {
+    'navigation': { enabled: true },
+    'graphql': {
+        enabled: true,
+        config: {
+            defaultLimit: 150,  // Définit une nouvelle limite par défaut (actuellement c'est 10)
+            maxLimit: 1000,     // Fixe une limite maximale possible pour éviter des abus
+        },
+    },
+};
